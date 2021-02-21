2a12f43863c0ae7228ec3aef823c10f8547334ad
U-LAPTOP-KJPKVBT4\miame <miamedra1@icloud.com>
2021-02-21 09:25:56 -0800

    Commit: Got chapter 13 stuff xadc stuff to work. 	modified:   ../chu_fpro/chu_fpro.sdk/mcs_top_vanilla.hdf 	new file:   chu_fpro_adc.sdk/cpu/src/chu_init.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/chu_init.h 	new file:   chu_fpro_adc.sdk/cpu/src/chu_io_map.h 	new file:   chu_fpro_adc.sdk/cpu/src/chu_io_rw.h 	new file:   chu_fpro_adc.sdk/cpu/src/gpio_cores.h 	new file:   chu_fpro_adc.sdk/cpu/src/gpios_cores.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/main_sampler_test.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/timer_core.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/timer_core.h 	new file:   chu_fpro_adc.sdk/cpu/src/uart_core.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/uart_core.h 	new file:   chu_fpro_adc.sdk/cpu/src/xadc_core.cpp 	new file:   chu_fpro_adc.sdk/cpu/src/xadc_core.h 	new file:   chu_fpro_adc.tcl 	new file:   src/baud_gen.sv 	new file:   src/chu_gpi.sv 	new file:   src/chu_gpo.sv 	new file:   src/chu_io_map.svh 	new file:   src/chu_mcs_bridge.sv 	new file:   src/chu_mmio_controller.sv 	new file:   src/chu_timer.sv 	new file:   src/chu_uart.sv 	new file:   src/cpu/cpu.bd 	new file:   src/fifo.sv 	new file:   src/fifo_ctrl.sv 	new file:   src/mcs_top_vanilla.sv 	new file:   src/mmio_sys_sampler.sv 	new file:   src/mmio_sys_vanilla.sv 	new file:   src/reg_file.sv 	new file:   src/uart.sv 	new file:   src/uart_rx.sv 	new file:   src/uart_tx.sv

---
+++ designs/chu_fpro_adc/chu_fpro_adc.sdk/cpu/src/main_sampler_test.cpp
@@ -0,0 +1,213 @@
+/*****************************************************************//**
+ * @file main_sampler_test.cpp
+ *
+ * @brief Basic test of nexys4 ddr mmio cores
+ *
+ * @author p chu
+ * @version v1.0: initial release
+ *********************************************************************/
+
+// #define _DEBUG
+#include "chu_init.h"
+#include "gpio_cores.h"
+#include "xadc_core.h"
+
+
+/**
+ * blink once per second for 5 times.
+ * provide a sanity check for timer (based on SYS_CLK_FREQ)
+ * @param led_p pointer to led instance
+ */
+void timer_check(GpoCore *led_p) {
+   int i;
+
+   for (i = 0; i < 5; i++) {
+      led_p->write(0xffff);
+      sleep_ms(500);
+      led_p->write(0x0000);
+      sleep_ms(500);
+      debug("timer check - (loop #)/now: ", i, now_ms());
+   }
+}
+
+/**
+ * check individual led
+ * @param led_p pointer to led instance
+ * @param n number of led
+ */
+void led_check(GpoCore *led_p, int n) {
+   int i;
+
+   for (i = 0; i < n; i++) {
+      led_p->write(1, i);
+      sleep_ms(100);
+      led_p->write(0, i);
+      sleep_ms(100);
+   }
+}
+
+/**
+ * leds flash according to switch positions.
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+void sw_check(GpoCore *led_p, GpiCore *sw_p) {
+   int i, s;
+
+   s = sw_p->read();
+   for (i = 0; i < 30; i++) {
+      led_p->write(s);
+      sleep_ms(50);
+      led_p->write(0);
+      sleep_ms(50);
+   }
+}
+
+/**
+ * uart transmits test line.
+ * @note uart instance is declared as global variable in chu_io_basic.h
+ */
+void uart_check() {
+   static int loop = 0;
+
+   uart.disp("uart test #");
+   uart.disp(loop);
+   uart.disp("\n\r");
+   loop++;
+}
+
+/**
+ * read FPGA internal voltage temperature
+ * @param adc_p pointer to xadc instance
+ */
+
+void adc_check(XadcCore *adc_p, GpoCore *led_p) {
+   double reading;
+   int n, i;
+   uint16_t raw;
+
+   for (i = 0; i < 5; i++) {
+      // display 12-bit channel 0 reading in LED
+      raw = adc_p->read_raw(0);
+      raw = raw >> 4;
+      led_p->write(raw);
+      // display on-chip sensor and 4 channels in console
+      uart.disp("FPGA vcc/temp: ");
+      reading = adc_p->read_fpga_vcc();
+      uart.disp(reading, 3);
+      uart.disp(" / ");
+      reading = adc_p->read_fpga_temp();
+      uart.disp(reading, 3);
+      uart.disp("\n\r");
+      for (n = 0; n < 4; n++) {
+         uart.disp("analog channel/voltage: ");
+         uart.disp(n);
+         uart.disp(" / ");
+         reading = adc_p->read_adc_in(n);
+         uart.disp(reading, 3);
+         uart.disp("\n\r");
+      } // end for
+      sleep_ms(200);
+   }
+}
+
+/**
+ * tri-color led dims gradually
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+
+void pwm_3color_led_check(PwmCore *pwm_p) {
+   int i, n;
+   double bright, duty;
+   const double P20 = 1.2589;  // P20=100^(1/20); i.e., P20^20=100
+
+   pwm_p->set_freq(50);
+   for (n = 0; n < 3; n++) {
+      bright = 1.0;
+      for (i = 0; i < 20; i++) {
+         bright = bright * P20;
+         duty = bright / 100.0;
+         pwm_p->set_duty(duty, n);
+         pwm_p->set_duty(duty, n + 3);
+         sleep_ms(100);
+      }
+      sleep_ms(300);
+      pwm_p->set_duty(0.0, n);
+      pwm_p->set_duty(0.0, n + 3);
+   }
+}
+
+/**
+ * Test debounced buttons
+ *   - count transitions of normal and debounced button
+ * @param db_p pointer to debouceCore instance
+ */
+
+void debounce_check(DebounceCore *db_p, GpoCore *led_p) {
+   long start_time;
+   int btn_old, db_old, btn_new, db_new;
+   int b = 0;
+   int d = 0;
+   uint32_t ptn;
+
+   start_time = now_ms();
+   btn_old = db_p->read();
+   db_old = db_p->read_db();
+   do {
+      btn_new = db_p->read();
+      db_new = db_p->read_db();
+      if (btn_old != btn_new) {
+         b = b + 1;
+         btn_old = btn_new;
+      }
+      if (db_old != db_new) {
+         d = d + 1;
+         db_old = db_new;
+      }
+      ptn = d & 0x0000000f;
+      ptn = ptn | (b & 0x0000000f) << 4;
+      led_p->write(ptn);
+   } while ((now_ms() - start_time) < 5000);
+}
+
+/**
+ * core test
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+void show_test_id(int n, GpoCore *led_p) {
+   int i, ptn;
+
+   ptn = n; //1 << n;
+   for (i = 0; i < 20; i++) {
+      led_p->write(ptn);
+      sleep_ms(30);
+      led_p->write(0);
+      sleep_ms(30);
+   }
+}
+
+GpoCore led(get_slot_addr(BRIDGE_BASE, S2_LED));
+GpiCore sw(get_slot_addr(BRIDGE_BASE, S3_SW));
+XadcCore adc(get_slot_addr(BRIDGE_BASE, S5_XDAC));
+PwmCore pwm(get_slot_addr(BRIDGE_BASE, S6_PWM));
+DebounceCore btn(get_slot_addr(BRIDGE_BASE, S7_BTN));
+
+
+int main() {
+   //uint8_t id, ;
+
+   timer_check(&led);
+   while (1) {
+      show_test_id(1, &led);
+      led_check(&led, 16);
+      sw_check(&led, &sw);
+      show_test_id(3, &led);
+      uart_check();
+      debug("main - switch value / up time : ", sw.read(), now_ms());
+      show_test_id(5, &led);
+      adc_check(&adc, &led);
+
+   }
+}
